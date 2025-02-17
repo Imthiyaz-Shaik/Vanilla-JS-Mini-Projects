6ffd4477aad19b1cf345d4c8a466c037563e8366
Imthiyaz-Shaik <20pp5a0275+github@gmail.com>
2025-02-17 19:26:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vanilla-JS-Mini-Projects
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Imthiyaz-Shaik/Vanilla-JS-Mini-Projects)
---
+++ Vanilla JS BG Color Changer Mini Proj/script.js
@@ -0,0 +1,38 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'black') {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = 'white';
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = 'black';
+    }
+    if (e.target.id === 'crimson') {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = 'beige';
+    }
+    if (e.target.id === 'lightyellow') {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = 'brown';
+    }
+    if (e.target.id === 'bisque') {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = 'darkslategrey';
+    }
+    if (e.target.id === 'chocolate') {
+      body.style.backgroundColor = e.target.id;
+    }
+
+    if (e.target.id === 'palegreen') {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = 'crimson';
+    }
+  });
+});
